fa336468e46e35dcbea6000c57e2cf59ae7bf4df
baerencode <69156548+barencode@users.noreply.github.com>
2026-09-26 00:19:52 +0300

    Extract configurable mining shifts function

--- main.go
+++ main.go
@@ -3,14 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	balance := 0
+	fmt.Println(mineShifts(5, 3, 2))
+	fmt.Println(mineShifts(5, 8, 2))
+	fmt.Println(mineShifts(1, 1, 2))
+	fmt.Println(mineShifts(5, 3, 0))
+}
 
-	for i := 1; i <= 5; i++ {
-		if i == 3 {
-			continue
-		}
-		balance += 2
+func mineShifts(shifts int, maintenanceShift int, production int) int {
+	if shifts <= 0 || production <= 0 {
+		return 0
 	}
 
-	fmt.Println(balance)
+	balance := 0
+
+	for ; shifts > 0; shifts-- {
+		if shifts == maintenanceShift {
+			continue
+		}
+		balance += production
+	}
+	return balance
 }
